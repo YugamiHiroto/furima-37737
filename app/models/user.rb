94ac58b12e5c3c4b5eb3e04168b0fe90067e8cfe
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-22 16:49:11 +0900

    エラーメッセージ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
 
-
-  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-z0-9]+\z/i }, allow_nil: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
   validates :family_name, presence: true
   validates :first_name, presence: true
+  end
+ 
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  end
 
 end
