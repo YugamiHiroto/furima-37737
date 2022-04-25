a0dacf31e6411eabc7783d0fe46d245f888239cd
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-25 15:53:29 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,14 @@ class User < ApplicationRecord
     validates :nickname
     validates :birth_date
   end
-  
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
     validates :family_name
     validates :first_name
   end
- 
+
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
     validates :family_name_kana
     validates :first_name_kana
   end
-
 end
