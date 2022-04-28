12f2b8e790680fc678b040768cb09f4d8d05cf87
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-28 18:33:31 +0900

    Status collection_select設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
-    
+    belongs_to :status
     has_one_attached :image
+    
     validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
