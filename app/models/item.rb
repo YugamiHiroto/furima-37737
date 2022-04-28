a5cbe261cf3caf3f35e5f6eadac60c203c64a34d
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-28 18:52:00 +0900

    shipping_day collection_select 設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,13 @@ class Item < ApplicationRecord
     belongs_to :category
     belongs_to :status
     belongs_to :shipping_cost
+    belongs_to :shipping_day
+
     has_one_attached :image
 
     validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
