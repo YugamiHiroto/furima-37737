d9031c3ebe58ce7306e1f2ff3fbf463269e81df4
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-28 18:40:21 +0900

    shipping_cost collection_select設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,10 @@ class Item < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :status
+    belongs_to :shipping_cost
     has_one_attached :image
-    
+
     validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
