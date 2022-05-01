f1d16db5c95b8a1686657496c50d978e9cc7ce20
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-05-01 17:11:41 +0900

    item_spec.rb テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,11 +62,59 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture can't be blank"
       end
-      it "priceが300~9999999の間でない場合は登録できないこと" do
+      it "priceが半角英数字混合では出品できない" do
         @item.image = fixture_file_upload('app/assets/images/flag.png')
-        @item.price = '10'
+        @item.price = '11aa'
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not included in the list"
+      end
+      it "priceが半角英字のみでは出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.price = 'aaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not included in the list"
+      end
+      it "priceが商品価格が全角文字では出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.price = 'あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not included in the list"
+      end
+      it "カテゴリーに{---}が選択されている場合は出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it "商品の状態に{---}が選択されている場合は出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status can't be blank"
+      end
+      it "配送料の負担に{---}が選択されている場合は出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.shipping_cost_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping cost can't be blank"
+      end
+      it "発送元の地域に{---}が選択されている場合は出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.prefecture_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it "発送までの日数に{---}が選択されている場合は出品できない" do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.shipping_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping day can't be blank"
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.image = fixture_file_upload('app/assets/images/flag.png')
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
