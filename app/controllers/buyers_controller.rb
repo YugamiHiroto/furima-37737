ba82c30a468d69533dd9b6e7135997daf54874ec
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-05-09 22:24:41 +0900

    buyerscontroller編集

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,6 @@
 class BuyersController < ApplicationController
+  before_action :set_item
+
     def index
         @item = Item.find(params[:item_id])
         @buyer_address = BuyerAddress.new
@@ -7,6 +9,7 @@ class BuyersController < ApplicationController
     def create
         @buyer_address = BuyerAddress.new(buyer_params)
         if @buyer_address.valid?
+           pay_item
            @buyer_address.save
            redirect_to root_path
         else
@@ -21,4 +24,16 @@ class BuyersController < ApplicationController
     params.require(:buyer_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :buyer_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_8ad547c5bd5d88e161b83193"
+          Payjp::Charge.create(
+          amount: @item.price,  
+          card: buyer_params[:token],   
+          currency: 'jpy'
+      )
+  end
 end
