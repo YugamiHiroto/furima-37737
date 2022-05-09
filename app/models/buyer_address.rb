b4fafd35022357245b60367b6c6c1e7c6ccdb2e2
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-05-09 22:35:02 +0900

    トークンテストコード

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -4,7 +4,7 @@ class BuyerAddress
     attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id , :item_id, :token
   
     with_options presence: true do
-      validates :user_id, :item_id, :address, :city
+      validates :user_id, :item_id, :address, :city, :token
       validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
       validates :phone_number,format: {with: /\A\d{10,11}\z/}
     end
